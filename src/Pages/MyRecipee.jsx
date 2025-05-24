d1c89b2c1b140af65234606804faaa6b14973e8b
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-05-24 21:12:26 +0600

    Data added to my recipee section

--- src/Pages/MyRecipee.jsx
+++ src/Pages/MyRecipee.jsx
@@ -6,7 +6,7 @@ import MyRecipeSingleComponent from '../components/MyRecipeSingleComponent';
 const MyRecipee = () => {
     const recipes = useLoaderData();
     return (
-        <div className='grid grid-cols-1 gap-4 mt-[100px] py-[100px] max-w-full md:max-w-5xl lg:max-w-7xl mx-auto'>
+        <div className='grid grid-cols-1 gap-4 max-w-full md:max-w-5xl lg:max-w-7xl mx-auto'>
             {
                 recipes.map(recipe => <MyRecipeSingleComponent key={recipe._id} recipe={recipe}></MyRecipeSingleComponent>)
             }
--- src/components/MyRecipeSingleComponent.jsx
+++ src/components/MyRecipeSingleComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const MyRecipeSingleComponent = ({recipe}) => {
+const MyRecipeSingleComponent = ({ recipe }) => {
     const { image, title, ingredients, instructions, cuisine, preparation, category, rating } = recipe
     return (
-        <div className="hero bg-base-200 min-h-screen py-[150px]">
+        <div className="hero bg-base-200 min-h-screen  md:py-[150px] lg:py-[150px]">
             <div className="hero-content flex-col lg:flex-row border-2 border-amber-300 rounded-2xl">
                 <img
                     src={image}
@@ -17,7 +17,11 @@ const MyRecipeSingleComponent = ({recipe}) => {
                     <p className="oswald text-gray-300 font-bold">Preparation: {preparation}</p>
                     <p className="oswald text-gray-300 font-bold">Category: <span className='tang text-amber-200 text-2xl'>{category}</span></p><br />
                     <p className="oswald text-gray-300 font-bold">Rating: <span className='text-amber-200 text-2xl'>{rating}</span></p><br />
-                    <button className="btn btn-primary">Like this Recipe</button>
+                    <div className="space-x-3">
+                        <button className="btn btn-primary ">Update</button>
+                        <button className="btn btn-primary">Delete</button>
+                    </div>
+
                 </div>
             </div>
         </div>
